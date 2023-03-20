de19fbba925ba553e461ae4a0c9f3ad99b89be35
Giovanni Capelli <giovacape01@gmail.com>
2023-03-20 11:36:57 +0100

    completo.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel 9 + Bootstrap Template</title>
+        <title>Treni di GC</title>
 
         {{-- Includiamo gli assets con la direttiva @vite --}}
         @vite('resources/js/app.js')
@@ -14,17 +14,20 @@
         <main>
             <div class="container">
                 <div class="row">
-                    <div class="col text-center">
+                    @foreach ($trains as $train)
+                    <div class="col-3 text-center">
 
                         <div class="card">
                             <div class="card-body">
-                                <img src="{{ Vite::asset('resources/img/laravel.png') }}" alt="">
-
-                                <h1 class="card-title">Laravel 9 + Bootstrap Template</h1>
+                                <h2>
+                                    {{ $train->departure_station }} - {{ $train->arrival_station }}
+                                </h2>
                             </div>
                         </div>
 
-                    </div>
+                    </div>  
+                    @endforeach
+
                 </div>
             </div>
         </main>
